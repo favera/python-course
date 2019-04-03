fcc9a426b45d4117957ec8835117458e8365b759
fveraMI <fvera@excelsis.com.py>
2019-04-03 15:46:16 -0400

    python course first commit

---
+++ basics.py
@@ -0,0 +1,22 @@
+#asignar una variable
+a = 10
+
+#multiple asignacion
+x, y, z = 1, 2, 3
+
+#function id posicion en memoria de la variable
+id(a)
+id(10)
+b = a
+id(b)
+b = 20
+id(b)
+
+#Basic Keywords
+#False      await      else       import     pass
+#None       break      except     in         raise
+#True       class      finally    is         return
+#and        continue   for        lambda     try
+#as         def        from       nonlocal   while
+#assert     del        global     not        with
+#async      elif       if         or         yield
---
+++ string.py
@@ -0,0 +1,10 @@
+string1 = "Cisco Router"
+
+#se puede acceder a los indices de un string, cada caracter representa un indice
+#el ultimo caracter de puede acceder con -1
+#para leer un string del reves se comineza con -1 y va decrenciendo en negativo
+string1[1]
+string1[-1]
+
+#para saber el lenght de un caracter usar la funcion len
+len(string1)
